015c9a1e9aa063493cfec7e982afb44d44a8f226
James Williams <jr_williams@me.com>
2021-04-19 18:43:59 +0100

    Added initial project interface

--- src/services/project.ts
+++ src/services/project.ts
@@ -1,3 +1,15 @@
-export interface IProject {
+import { Content, IImage } from './content';
+import { IExternalLink } from './organisation';
 
+export interface IProject {
+  id: string
+  organisationId: string
+  name: string
+  description: string
+  hero?: IImage
+  about?: Content
+  externalLinks?: IExternalLink[]
+  /** TopicTags */ tags?: string[]
+  /** PersonId[] */ people: string[]
+  /** PersonId */ primaryContact: string
 }
